0491f28087b4b8c9be9219c1f6098283f11b228f
Dipak <125576159+DipakShinde1196@users.noreply.github.com>
2024-10-13 23:12:33 +0530

    Update signals.py

--- tasktwo/signals.py
+++ tasktwo/signals.py
@@ -1,11 +1,8 @@
-# signals.py
 from django.dispatch import Signal, receiver
 import threading
 
-# Define a custom signal
 my_signal = Signal()
 
-# Define a receiver function
 @receiver(my_signal)
 def my_receiver(sender, **kwargs):
     print(f"Receiver running in thread: {threading.current_thread().name}")
